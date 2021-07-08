f0d27415c866c8a15969493d2970caa653334456
nnn2894@naver.com <nnn2894@naver.com>
2021-07-08 20:36:22 +0900

    0708

--- main.py
+++ main.py
@@ -127,24 +127,34 @@ if __name__ == "__main__":  # 이 파일을 직접 실행했을 경우 __name__ 
                     center_x2 = int((center_min_x + center_max_x) / 2)
                     center_y2 = int((center_min_y + center_max_y) / 2)
 
+                    if center_x2 > 640:
+                        center_x2 = 640
+                    if center_x2 < 0:
+                        center_x2 = 0
+
+                    if center_y2 > 480:
+                        center_y2 = 480
+                    if center_y2 < 0:
+                        center_y2 = 0
+
                     if center_x2 < 310:  # 중점이 왼쪽에 있다. -> 왼쪽으로 가야한다.
                         drone.sendControlPosition16(0, 1, 0, 1, 0, 0)
-                        sleep(0.5)
+                        sleep(1)
                         print("go to left")
                     elif center_x2 > 330:  # 중점이 오른쪽에 있다. -> 오른쪽으로 가야한다.
                         drone.sendControlPosition16(0, -1, 0, 1, 0, 0)
-                        sleep(0.5)
+                        sleep(1)
                         print("go to right")
                     elif center_x2 >= 310 and center_x2 <= 330:
                         check[0] = 1
 
                     if center_y2 < 230:  # 중점이 아래에있다 - > 위로 가야한다.
                         drone.sendControlPosition16(0, 0, 1, 1, 0, 0)
-                        sleep(0.5)
+                        sleep(1)
                         print("go to donw")
                     elif center_y2 > 250:  # 중점이 위에 있다. -> 아래로 가야한다.
                         drone.sendControlPosition16(0, 0, -1, 1, 0, 0)
-                        sleep(0.5)
+                        sleep(1)
                         print("go to up")
                     elif center_y2 >= 230 and center_y2 <= 250:
                         check[1] = 1
