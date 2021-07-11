2e0cbb4e5ab7c49d4ea943c265cc0614b9f9f14a
nnn2894@naver.com <nnn2894@naver.com>
2021-07-11 23:34:27 +0900

    0710

--- main.py
+++ main.py
@@ -321,7 +321,7 @@ if __name__ == "__main__":  # 이 파일을 직접 실행했을 경우 __name__ 
                 if phase_1_2 == 1:
                     sleep(5)
                     blue_num_pixel = np.sum(np.where(bi_blue[:][:] > 0, 1, 0))
-                    if blue_num_pixel > 500:
+                    if blue_num_pixel > 130000:
                         print("need to back")
                         print(f"pixel_num is {blue_num_pixel}")
                         drone.sendControlPosition16(-4, 0, 0, 5, 0, 0)
